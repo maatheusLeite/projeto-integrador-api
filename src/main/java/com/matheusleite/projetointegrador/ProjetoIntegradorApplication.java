fecab931c646ad03501c990828c7e05d0481ec8a
Matheus Leite <maatheus2710@gmail.com>
2023-02-01 09:10:21 -0300

    Project created

---
+++ src/main/java/com/matheusleite/projetointegrador/ProjetoIntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.matheusleite.projetointegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegradorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusleite/projetointegrador/ProjetoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusleite.projetointegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
